588d5d1344e91d1bd69e4dcbdb28b524fa150998
Jonathan Walker <walke954@umn.edu>
2018-10-15 18:49:22 -0700

    updated

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -222,32 +222,37 @@ function refreshToken(){
 
 function logIn(){
 	// get user information
-	$.ajax({
-		url: '/users/logged',
-		type: 'GET',
-		beforeSend: function(xhr){
-			xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem('prjToken')}`);
-		},
-		success: function(data){
-			refreshToken();
+	if(localStorage.getItem('prjToken') !== null){
+		$.ajax({
+			url: '/users/logged',
+			type: 'GET',
+			beforeSend: function(xhr){
+				xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem('prjToken')}`);
+			},
+			success: function(data){
+				refreshToken();
 
-			profile_basics = data;
-			month_profile_created = data.monthCreated;
-			year_profile_created = data.yearCreated;
+				profile_basics = data;
+				month_profile_created = data.monthCreated;
+				year_profile_created = data.yearCreated;
 
-			loadAppHome();
+				loadAppHome();
 
-			underlinePageLabel('#home-page');
-		},
-		error: function(){
-			$('main').html(loginPageElement());
-			$('#header-right').html(`
-				<p id="about-page" tabindex="0" role="button">About</p>
-				<p id="login-page" tabindex="0" role="button">Login</p>
-			`);
-			underlinePageLabel($('#login-page'));
-		}
-	});
+				underlinePageLabel('#home-page');
+			},
+			error: function(){
+				$('main').html(loginPageElement());
+				$('#header-right').html(`
+					<p id="about-page" tabindex="0" role="button">About</p>
+					<p id="login-page" tabindex="0" role="button">Login</p>
+				`);
+				underlinePageLabel($('#login-page'));
+			}
+		});
+	}
+	else{
+		$('main').html(loginPageElement());
+	}
 }
 
 function initApp(){
@@ -255,7 +260,7 @@ function initApp(){
 		for(let i = 0; i < data.prompts.length; i++){
 			prompts.push(data.prompts[i]);
 		}
-
+		
 		logIn();
 	});
 }
